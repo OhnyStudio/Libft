f38556f2d6cdb053197734c38017d476fb715337
Johny Savard <jsavard@c1r8p8.42quebec.com>
2022-11-09 14:49:42 -0500

    lstmap strict OK

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,38 +6,33 @@
 /*   By: jsavard <jsavard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 14:37:17 by jsavard           #+#    #+#             */
-/*   Updated: 2022/11/09 14:26:38 by jsavard          ###   ########.fr       */
+/*   Updated: 2022/11/09 14:43:27 by jsavard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static t_list	*ft_error(t_list **lst, void (*del)(void *))
-{
-	ft_lstclear(lst, del);
-	return (0);
-}
-
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new;
-	t_list	*save;
+	t_list	*new_list;
+	t_list	*new_item;
+	void	*content;
 
 	if (!lst || !f || !del)
 		return (NULL);
-	new = ft_lstnew((*f)(lst->content));
-	if (!new)
-		return (ft_error(&new, del));
-	save = new;
-	lst = lst->next;
+	new_list = NULL;
 	while (lst)
 	{
-		new->next = ft_lstnew((*f)(lst->content));
-		if (!new->next)
-			return (ft_error(&new, del));
+		content = (*f)(lst->content);
+		new_item = ft_lstnew(content);
+		if (new_item == NULL)
+		{
+			ft_lstclear(&new_list, del);
+			free(content);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, new_item);
 		lst = lst->next;
-		new = new->next;
 	}
-	new->next = NULL;
-	return (save);
+	return (new_list);
 }
