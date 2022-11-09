70a3b15de43a8ca0ff751b8e171450100c7514a3
Johny Savard <jsavard@c1r8p8.42quebec.com>
2022-11-09 13:52:19 -0500

    split strict OK

--- ft_split.c
+++ ft_split.c
@@ -6,12 +6,11 @@
 /*   By: jsavard <jsavard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 14:39:04 by jsavard           #+#    #+#             */
-/*   Updated: 2022/11/09 13:05:39 by jsavard          ###   ########.fr       */
+/*   Updated: 2022/11/09 13:50:55 by jsavard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "stdio.h"
 
 static int	ft_count_split(char const *s, char c)
 {
@@ -36,6 +35,25 @@ static int	ft_count_split(char const *s, char c)
 	return (i);
 }
 
+static void	*ft_free(char **strs, int j)
+{
+	while (j-- > 0)
+		free(strs[j]);
+	free(strs);
+	return (NULL);
+}
+
+static void	*ft_make_word(const char *s, int start, int finish)
+{
+	char	*temp;
+
+	temp = ft_calloc(sizeof(char), (finish - start + 1));
+	if (!temp)
+		return (NULL);
+	ft_strlcpy(temp, &s[start], (finish - start) + 1);
+	return (temp);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	size_t	i;
@@ -46,47 +64,21 @@ char	**ft_split(char const *s, char c)
 	strs = ft_calloc(sizeof(char *), (ft_count_split(s, c) + 1));
 	if (!strs || !s)
 		return (NULL);
-	i = 0;
+	i = -1;
 	j = 0;
 	tmp = -1;
-	while (i <= ft_strlen(s))
+	while (++i <= ft_strlen(s))
 	{
 		if (s[i] != c && tmp < 0)
 			tmp = i;
 		else if ((s[i] == c || i == ft_strlen(s)) && tmp >= 0)
 		{
-			strs[j] = ft_calloc((i - tmp + 1), sizeof(char));
-			ft_strlcpy(strs[j++], &s[tmp], (i - tmp) + 1);
+			strs[j] = ft_make_word(s, tmp, i);
+			if (strs[j++] == NULL)
+				return (ft_free(strs, j));
 			tmp = -1;
 		}
-		i++;
 	}
 	strs[j] = 0;
 	return (strs);
 }
-
-/*int	main(void)
-{
-	char	**strs;
-	int		i;
-
-	strs = ft_split("hello!", ' ');
-	i = 0;
-	while (strs[i])
-	{
-		printf("%s\n", strs[i]);
-		free(strs[i]);
-		i++;
-	}
-	free(strs);
-	strs = ft_split("hello! test", ' ');
-	i = 0;
-	while (strs[i])
-	{
-		printf("%s\n", strs[i]);
-		free(strs[i]);
-		i++;
-	}
-	free(strs);
-	return (0);
-}*/
