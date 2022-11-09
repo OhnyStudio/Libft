f6ce3c1c5a7832365baab10105465eebdbdab96f
Johny Savard <jsavard@c1r8p8.42quebec.com>
2022-11-09 14:28:47 -0500

    lstmap modifié

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -3,36 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: johnysavard <johnysavard@student.42.fr>    +#+  +:+       +#+        */
+/*   By: jsavard <jsavard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 14:37:17 by jsavard           #+#    #+#             */
-/*   Updated: 2022/10/30 09:44:43 by johnysavard      ###   ########.fr       */
+/*   Updated: 2022/11/09 14:26:38 by jsavard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static t_list	*ft_error(t_list **lst, void (*del)(void *))
+{
+	ft_lstclear(lst, del);
+	return (0);
+}
+
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
-	t_list	*first;
+	t_list	*save;
 
-	first = NULL;
+	if (!lst || !f || !del)
+		return (NULL);
+	new = ft_lstnew((*f)(lst->content));
+	if (!new)
+		return (ft_error(&new, del));
+	save = new;
+	lst = lst->next;
 	while (lst)
 	{
-		new = ft_lstnew((*f)(lst->content));
-		if (!new)
-		{
-			while (first)
-			{
-				new = first->next;
-				(*del)(first->content);
-				free(first);
-				first = new;
-			}
-		}
-		ft_lstadd_back(&first, new);
+		new->next = ft_lstnew((*f)(lst->content));
+		if (!new->next)
+			return (ft_error(&new, del));
 		lst = lst->next;
+		new = new->next;
 	}
-	return (first);
+	new->next = NULL;
+	return (save);
 }
